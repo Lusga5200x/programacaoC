fc8ebf707b4dd6cd073eb839458c888e1cf46db5
Lusga5200x <eolcbr@outlook.com>
2026-03-05 17:52:07 -0300

    Create variaveis.c

---
+++ variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    int idade = 22;
+    int quantidade = 2;
+    float altura = 1.70; 
+    double peso = 80.0;
+    char letra = 'L';
+    char nome[20] = "Lucas";
+
+}
