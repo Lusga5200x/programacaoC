d3f08633479f555a93c915a884502044362e8dfc
Lusga5200x <eolcbr@outlook.com>
2026-03-05 20:06:57 -0300

    saida_de_dados.c

---
+++ saida_de_dados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 22;
+    char nome[20] = "Lucas";
+    float altura = 1.70;
+    char opcao = 'm';
+
+    printf("A idade é: %i\nO nome é: %s\nA altura é: %.2f\nE a opção é: %c", idade, nome, altura, opcao);
+
+
+}
